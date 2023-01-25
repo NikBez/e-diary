f5a2323671bff3d4c351d0a4e39e0f31e13faece
Kit <nikitabit@yandex.ru>
2023-01-25 10:54:09 +0700

    Make cosmetic changes Add function get_subject Make COMMENDATION_TEXT - Global Remove * from import

--- script.py
+++ script.py
@@ -1,43 +1,36 @@
-from datacenter.models import *
+from datacenter.models import Mark, Chastisement, Lesson, Schoolkid, Commendation, Subject
 from random import choice
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
+COMMENDATION_TEXT = [
+    "Гораздо лучше, чем я ожидал!",
+    "Ты меня приятно удивил!",
+    "Великолепно!",
+    "Ты меня очень обрадовал!",
+    "Именно этого я давно ждал от тебя!",
+    "Сказано здорово – просто и ясно!",
+    "Ты, как всегда, точен!",
+    "Ты сегодня прыгнул выше головы!",
+    "С каждым разом у тебя получается всё лучше!",
+]
 
 def fix_marks(schoolkid):
 
-    kid = get_schoolkit(schoolkid)
-    bad_marks = Mark.objects.filter(schoolkid = kid, points__lte=3)
-    for mark in bad_marks:
-        mark.points = 5
-        mark.save()
+    kid = get_schoolkid(schoolkid)
+    Mark.objects.filter(schoolkid = kid, points__lte=3).update(points=5)
     print("All done")
 
 def remove_chastisements(schoolkid):
 
-    kid = get_schoolkit(schoolkid)
-    chastisements = Chastisement.objects.filter(schoolkid=kid)
-    for chais in chastisements:
-        chais.delete()
+    kid = get_schoolkid(schoolkid)
+    Chastisement.objects.filter(schoolkid=kid).delete()
     print("All done")
-
 
 def create_commendations(schoolkid, subject_to_commendate):
 
-    commendation_text=[
-        "Гораздо лучше, чем я ожидал!",
-        "Ты меня приятно удивил!",
-        "Великолепно!",
-        "Ты меня очень обрадовал!",
-        "Именно этого я давно ждал от тебя!",
-        "Сказано здорово – просто и ясно!",
-        "Ты, как всегда, точен!",
-        "Ты сегодня прыгнул выше головы!",
-        "С каждым разом у тебя получается всё лучше!",
-    ]
-
-    kid = get_schoolkit(schoolkid)
-    lesson = Lesson.objects.filter(year_of_study=6, group_letter="А", subject__title=subject_to_commendate).order_by("date").first()
-    Commendation.objects.create(text=choice(commendation_text),
+    kid = get_schoolkid(schoolkid)
+    subject_title = get_subject(subject_to_commendate)
+    lesson = Lesson.objects.filter(year_of_study=6, group_letter="А", subject__title=subject_title).order_by("date").first()
+    Commendation.objects.create(text=choice(COMMENDATION_TEXT),
                                 created=lesson.date, schoolkid=kid,
                                 subject=lesson.subject,
                                 teacher=lesson.teacher
@@ -45,16 +38,20 @@ def create_commendations(schoolkid, subject_to_commendate):
     print("All done")
 
 
-def get_schoolkit(schoolkid):
-
+def get_schoolkid(schoolkid):
     try:
         return Schoolkid.objects.get(full_name__contains=schoolkid)
-    except ObjectDoesNotExist:
+    except DoesNotExist:
         print(f'Учеников по запросу "{schoolkid}" не найдено')
     except MultipleObjectsReturned:
         print(f' Найдено несколько учеников по запросу "{schoolkid}"')
 
 
-
-
+def get_subject(subject_title):
+    try:
+        return Subject.objects.get(title=subject_title)
+    except DoesNotExist:
+        print(f'Предмета по запросу "{subject_title}" не найдено')
+    except MultipleObjectsReturned:
+        print(f' Найдено несколько предметов по запросу "{subject_title}"')
 
